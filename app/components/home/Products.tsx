10a4a82fc83b03bcb13c3e8809279f830dbc47e2
Muhammad Alam <malam53345@gmail.com>
2024-01-18 01:07:15 +0500

    products section completed

---
+++ app/components/home/Products.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import Wrapper from '../shared/Wrapper'
+import Image from 'next/image'
+import { FaArrowRightLong } from "react-icons/fa6";
+export default function Products() {
+    return (
+        <Wrapper style='py-[4rem]'>
+            <div className="">
+                <h2 className="text-[34px] text-white font-[500] leading-[48px] text-center max-w-[400px] mx-auto mb-[3rem]">Unparalleled liquidity. Rock solid products.</h2>
+                <div className="flex flex-col gap-[1rem]">
+                    <div className="w-full rounded-[32px] flex flex-col md:flex-row justify-between" style={{ backgroundImage: 'linear-gradient(255.71deg, #5099CC 0.96%, rgba(44, 85, 114, 0) 78.36%)' }}>
+                        <div className="lg:w-[40%] flex flex-col gap-[10px] justify-center p-[2rem]">
+                            <h2 className="text-[28px] sm:text-[34px] font-[600] text-white leading-[40px] sm:leading-[48px]">Token Vaults</h2>
+                            <p className="text-[14px] font-[500] leading-[26px] text-white opacity-70">Lorem ipsum dolor sit amet consectetur. In magnis viverra nec mauris tempus feugiat platea et porttitor. In ultrices sem nulla massa. Mauris aliquet dis sit pulvinar proin facilisi.</p>
+                            <div className="flex items-center gap-[1rem] mt-[1rem]">
+                                <p className="text-[16px] font-[500] text-[#5099CC]">Explore Vaults</p>
+                                <FaArrowRightLong className='text-[#5099CC]' />
+                            </div>
+                        </div>
+                        <div className="relative hidden lg:flex justify-end">
+                            <Image src="/images/tokenbottom.svg" width={4000} height={400} alt='' className='w-full h-full rounded-br-[32px]' />
+                            <Image src="/images/tokenupper.png" width={400} height={380} alt='' className='absolute right-0 top-0 w-full h-full' />
+                        </div>
+                    </div>
+                    <div className="w-full flex flex-col lg:flex-row gap-[1rem]">
+                        <div className="grid grid-cols-1 lg:grid-cols-2 gap-[1rem] bg-[#5099CC7A] rounded-[32px] lg:w-[58%]">
+                            <div className="hidden lg:flex">
+                                <Image src="/images/analytics.svg" width={270} height={400} alt='' className='rounded-l-[32px] h-full' />
+                            </div>
+                            <div className="flex flex-col gap-[10px] justify-center p-[2rem]">
+                                <h2 className="text-[28px] sm:text-[34px] font-[600] text-white leading-[40px] sm:leading-[48px]">Data Analytics</h2>
+                                <p className="text-[14px] font-[500] leading-[26px] text-white opacity-70">Lorem ipsum dolor sit amet consectetur. In magnis viverra nec mauris tempus feugiat platea et porttitor. In ultrices sem nulla massa. Mauris aliquet dis sit pulvinar proin facilisi.</p>
+                                <div className="flex items-center gap-[1rem] mt-[1rem]">
+                                    <p className="text-[16px] font-[500] text-white">See Markets</p>
+                                    <FaArrowRightLong className='text-white' />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="bg-[#091620] lg:w-[42%] grid lg:grid-cols-3 rounded-[32px] relative">
+                            <div className="p-[2rem] lg:col-span-2 flex flex-col gap-[1rem]">
+                                <h2 className="text-[28px] sm:text-[34px] font-[600] text-white leading-[40px] sm:leading-[48px]">Trading Bot</h2>
+                                <p className="text-[14px] font-[500] leading-[26px] text-white opacity-70">Lorem ipsum dolor sit amet consectetur. In magnis viverra nec mauris tempus feugiat platea et porttitor. In ultrices sem nulla massa. Mauris aliquet dis sit pulvinar proin.</p>
+                                <button className='mt-[1rem] w-fit py-[8px] px-[12px] rounded-[12px] bg-[#5099CC14] text-white'>Start your own</button>
+                            </div>
+                            <div className="hidden lg:flex">
+                                <Image src="/images/bot.svg" width={150} height={285} alt='' className='absolute right-0 bottom-0 rounded-br-[32px]' />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Wrapper>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from './components/home/Hero'
 import Banner from './components/home/Banner'
+import Products from './components/home/Products'
 export default function page() {
   return (
     <>
       <Hero />
       <Banner />
+      <Products />
     </>
   )
 }
