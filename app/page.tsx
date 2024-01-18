921ac43c5b0c89c1d3a1f4ad21b417831658cf37
Muhammad Alam <malam53345@gmail.com>
2024-01-18 18:11:30 +0500

    faq section completed

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from './components/home/Hero'
 import Products from './components/home/Products'
 import Steps from './components/home/Steps'
 import Api from './components/home/Api'
+import Faq from './components/home/Faq'
 export default function page() {
   return (
     <>
@@ -10,6 +11,7 @@ export default function page() {
       <Products />
       <Steps />
       <Api />
+      <Faq />
     </>
   )
 }
